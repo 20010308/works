81ab7c6df7ea2703bb10b2465c533aa14be044aa
20010308 <xolmuminovumidjon0@gmail.com>
2021-06-19 19:11:03 +0500

    the end

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css"
-import {applyMiddleware, compose, createStore} from "redux";
+import {createStore} from "redux";
 import {rootReducer} from "./reducer/rootReducer";
-import thunk from "redux-thunk";
 import App from './App';
 
-const store =createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store =createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
